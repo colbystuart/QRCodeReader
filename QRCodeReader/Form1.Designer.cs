24a9d95882243435cd5d97b296c693e1e5e50d50
Colby Stuart <stuart_colby@yahoo.com>
2025-02-07 00:49:32 -0600

    Add project files.

---
+++ QRCodeReader/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace QRCodeReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Capture";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(727, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(284, 360);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(232, 36);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_Clicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Code Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ QRCodeReader/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+using IronQr;
+using IronSoftware.Drawing;
+using SixLabors.Fonts.Unicode;
+
+namespace QRCodeReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true; // Ensures the form stays on top
+            this.BackColor = System.Drawing.Color.LimeGreen; // Set a color that won't be visible
+            this.TransparencyKey = System.Drawing.Color.LimeGreen; // Make that color transparent
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // You can perform additional setup here if needed
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the form's position and size in screen coordinates
+            int startX = (int)(this.Left + this.Bounds.Left * 1.3);
+            int startY = (int)(this.Top * 2.53);
+            int width = (int)(this.Width * 2 * 1.1);
+            int height = (int)(this.Height * 2 * 1.045);
+
+            // Create a new bitmap with the form's dimensions
+            using (Bitmap bm = new Bitmap(width, height, PixelFormat.Format32bppRgb))
+            {
+                using (Graphics g = Graphics.FromImage(bm))
+                {
+                    // Capture the screen area exactly where the form is positioned
+                    g.CopyFromScreen(new System.Drawing.Point(startX, startY), System.Drawing.Point.Empty, new System.Drawing.Size(width, height));
+                }
+
+                // Dispose of old background image before assigning a new one
+                if (this.BackgroundImage != null)
+                {
+                    this.BackgroundImage.Dispose();
+                }
+
+                // Set the new captured image
+                this.BackgroundImage = (Bitmap)bm.Clone();
+
+                // Adjust form appearance
+                this.BackColor = System.Drawing.Color.LightGray;
+                button2.Visible = true;
+
+                // Save the image
+                string filePath = "MyImage.bmp";
+                bm.Save(filePath, ImageFormat.Bmp);
+
+                //Read QR Code
+
+                var inputBmp = AnyBitmap.FromFile("MyImage.bmp");
+
+                QrImageInput impageInput = new QrImageInput(inputBmp);
+
+                QrReader reader = new QrReader();
+
+                IEnumerable<QrResult> results = reader.Read(impageInput);
+
+                richTextBox1.Visible = true;
+                richTextBox1.Text = "";
+
+                foreach (var result in results)
+                {
+                    string value = result.Value;
+                    int start = richTextBox1.TextLength; // To get the current position in the text box
+                    richTextBox1.AppendText(value + Environment.NewLine);
+                    richTextBox1.Select(start, value.Length);
+                    richTextBox1.SelectionColor = System.Drawing.Color.Blue;
+                    richTextBox1.SelectionFont = new System.Drawing.Font(richTextBox1.Font, System.Drawing.FontStyle.Underline);
+                    richTextBox1.DeselectAll();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.BackgroundImage == null)
+            {
+                return;
+            }
+            else
+            {
+                this.BackColor = System.Drawing.Color.LimeGreen;
+                this.BackgroundImage = null;
+
+                if (System.IO.File.Exists("MyImage.bmp"))
+                {
+                    System.IO.File.Delete("MyImage.bmp");
+                }
+
+                button2.Visible = false;
+                richTextBox1.Text = "";
+                richTextBox1.Visible = false;
+            }
+        }
+
+        private void richTextBox1_Clicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+    }
+}
